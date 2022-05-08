4adbac1c1af643c128983cb50ade16670c6d9a82
d0kdenya <denden2100.nn@mail.ru>
2022-05-08 13:18:40 +0300

    Composition Task

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Users from "./components/users";
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Users />
+      <App />
   </React.StrictMode>
 );
 
